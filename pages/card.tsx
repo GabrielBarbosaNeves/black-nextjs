d9741e357ee583a2d98ab5faed14ddb3a85c60bb
gbneves@universal.org <gbneves@universal.org.br>
2023-06-29 11:50:10 -0300

    Estrutura de um Projeto Nextjs

---
+++ pages/card.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Card() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Meu carrinho de compras" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Carrinho</h1>
+    </>
+  );
+}
---
+++ pages/products.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Products() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Conheça todos os nossos produtos" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Nossos Produtos</h1>
+    </>
+  );
+}
